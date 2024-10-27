f79a56325ec3a2ed4fc2204a548f9d9d96ddf907
MukTuk101 <116485236+MukTuk101@users.noreply.github.com>
2024-10-27 13:08:50 +0800

    Add files via upload

--- TEGCN/scripts/EVAL_V2.sh
+++ TEGCN/scripts/EVAL_V2.sh
@@ -7,9 +7,9 @@ MODELNAME=runs/$RECORD
 #CONFIG=./config/uav-cross-subjectv1/test.yaml
 CONFIG=./config/uav-cross-subjectv2/test.yaml
 
-WEIGHTS=runs/2102-49-14650.pt
+WEIGHTS=runs/2102-36-10841.pt
 
 
 BATCH_SIZE=128
 
-python3 main.py  --config $CONFIG --work-dir $WORKDIR -model_saved_name $MODELNAME --device 0 --batch-size $BATCH_SIZE --test-batch-size $BATCH_SIZE --weights $WEIGHTS
+python3 main.py  --config $CONFIG --work-dir $WORKDIR -model_saved_name $MODELNAME --device 0 1 --batch-size $BATCH_SIZE --test-batch-size $BATCH_SIZE --weights $WEIGHTS
